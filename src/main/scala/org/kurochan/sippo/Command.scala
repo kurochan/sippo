4b6666b764b41b1317a32ddf939c73017b82713c
kurochan <kuro@kurochan.org>
2015-07-15 13:48:18 +0900

    add logger

--- src/main/scala/org/kurochan/sippo/Command.scala
+++ src/main/scala/org/kurochan/sippo/Command.scala
@@ -1,41 +1,42 @@
 package org.kurochan.sippo
 
-import java.io.{InputStream, OutputStream}
+import java.io._
 
 import org.apache.sshd.server.{Command => CommandOrig, Environment, ExitCallback}
+import com.typesafe.scalalogging.slf4j.StrictLogging
 
-class Command extends CommandOrig {
+class Command extends CommandOrig with StrictLogging {
   var exitCallback: Option[ExitCallback] = None
   var in: Option[InputStream] = None
   var out: Option[OutputStream] = None
   var err: Option[OutputStream] = None
 
   def destroy(): Unit = {
-    println("##### destroy!! #####")
+    logger.info("##### destroy!! #####")
   }
 
   def setInputStream(in: InputStream): Unit = {
-    println("##### set input!! #####")
+    logger.info("##### set input!! #####")
     this.in = Option(in)
   }
 
   def setErrorStream(err: OutputStream): Unit = {
-    println("##### set error!! #####")
+    logger.info("##### set error!! #####")
     this.err = Option(err)
   }
 
   def setOutputStream(out: OutputStream): Unit = {
-    println("##### set output!! #####")
+    logger.info("##### set output!! #####")
     this.out = Option(out)
   }
 
   def start(env: Environment): Unit = {
-    println("##### start!! #####")
+    logger.info("##### start!! #####")
     exitCallback.map(_.onExit(0, "bye"))
   }
 
   def setExitCallback(callback: ExitCallback): Unit = {
     exitCallback = Some(callback)
-    println("##### exit!! #####")
+    logger.info("##### exit!! #####")
   }
 }
