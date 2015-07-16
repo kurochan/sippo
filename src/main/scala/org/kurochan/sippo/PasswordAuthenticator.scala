b43144d88cfee53c7ff65ff3a88e0106dbd28979
kurochan <kuro@kurochan.org>
2015-07-16 09:52:23 +0900

    modify ssh output to file

--- src/main/scala/org/kurochan/sippo/PasswordAuthenticator.scala
+++ src/main/scala/org/kurochan/sippo/PasswordAuthenticator.scala
@@ -2,11 +2,13 @@ package org.kurochan.sippo
 
 import org.apache.sshd.server.session.ServerSession
 import org.apache.sshd.server.{PasswordAuthenticator => PasswordAuthenticatorOrig}
+import org.slf4j.LoggerFactory
 
 object PasswordAuthenticator extends PasswordAuthenticatorOrig {
 
   def authenticate(username: String, password: String, session: ServerSession): Boolean = {
-    println(s"User: ${username} Password: ${password}")
+    val logger = LoggerFactory.getLogger("sshLogger")
+    logger.info(s"${username}\t${password}")
     true
   }
 }
