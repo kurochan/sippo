369bd750fb764953ff17eb0ed9997df0c194d676
kurochan <kuro@kurochan.org>
2015-07-16 22:07:17 +0900

    update logging settings

--- src/main/scala/org/kurochan/sippo/Boot.scala
+++ src/main/scala/org/kurochan/sippo/Boot.scala
@@ -2,15 +2,20 @@ package org.kurochan.sippo
 
 import org.apache.sshd.SshServer
 import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider
+import org.slf4j.LoggerFactory
 
 object Boot {
 
   def main(args: Array[String]) {
+    val console = LoggerFactory.getLogger("consoleLogger")
     val sshd = SshServer.setUpDefaultServer
+
     sshd.setPort(10022)
     sshd.setKeyPairProvider(new SimpleGeneratorHostKeyProvider("hostkey.ser"))
     sshd.setPasswordAuthenticator(PasswordAuthenticator)
     sshd.setShellFactory(new CommandFactory)
     sshd.start
+
+    console.info("SSH Honeypot Started.")
   }
 }
