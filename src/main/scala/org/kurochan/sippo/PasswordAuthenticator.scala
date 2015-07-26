72f690346f9662cbeb569ec53e4331c8c87af37b
kurochan <kuro@kurochan.org>
2015-07-26 18:04:10 +0900

    add ip/port logging

--- src/main/scala/org/kurochan/sippo/PasswordAuthenticator.scala
+++ src/main/scala/org/kurochan/sippo/PasswordAuthenticator.scala
@@ -1,5 +1,7 @@
 package org.kurochan.sippo
 
+import java.net.InetSocketAddress
+
 import org.apache.sshd.server.session.ServerSession
 import org.apache.sshd.server.{PasswordAuthenticator => PasswordAuthenticatorOrig}
 import org.slf4j.LoggerFactory
@@ -8,7 +10,12 @@ object PasswordAuthenticator extends PasswordAuthenticatorOrig {
 
   def authenticate(username: String, password: String, session: ServerSession): Boolean = {
     val logger = LoggerFactory.getLogger("sshLogger")
-    logger.info(s"${username}\t${password}")
+
+    val socket = session.getIoSession.getRemoteAddress.asInstanceOf[InetSocketAddress]
+    val ip = socket.getAddress.getHostAddress
+    val port = socket.getPort
+
+    logger.info(s"${ip}\t${port}\t${username}\t${password}")
     true
   }
 }
